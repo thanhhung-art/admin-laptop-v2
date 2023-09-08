a2fabdf33c15806d76256f61a7900d10032f86f5
thanhhung <thanhhungdeveloper@gmail.com>
2023-09-08 13:57:42 +0700

    add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ReactQueryProvider from '@/providers/reactQueryProvider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>
+          {children}
+        </ReactQueryProvider>
+      </body>
     </html>
   )
 }
---
+++ src/lib/axios.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const Fetch = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  withCredentials: true
+})

6f4e0680f9f96eac8d865817004fe97b3f3582a6
thanhhung <thanhhungdeveloper@gmail.com>
2024-01-29 19:53:41 +0700

    add function upload image

---
+++ src/lib/cloundinary/uploadImage.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+export async function uploadImage(img: string) {
+  'user server'
+  const result = await cloudinary.uploader.upload(img);
+  return result;
+}

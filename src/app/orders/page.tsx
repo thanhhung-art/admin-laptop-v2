863b9f25d16c44ac1ece93066c0dbd7cbfba28c9
thanhhung <thanhhungdeveloper@gmail.com>
2024-01-29 19:54:53 +0700

    add page orders

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,27 @@
+import { ReactQueryHydrate } from "@/components/ReactQueryHydrate";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import { getOrders } from "@/lib/axios";
+import { queryClient } from "@/lib/reactQuery/queryClient";
+import OrdersPage from "@/views/Orders";
+import { dehydrate } from "@tanstack/react-query";
+
+const page = async () => {
+  const queryClientLocal = queryClient();
+  await queryClientLocal.prefetchQuery(["getOrders"], getOrders);
+  const dehydratedState = dehydrate(queryClientLocal);
+
+  return (
+    <ReactQueryHydrate state={dehydratedState}>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-1">
+          <OrdersPage />
+        </div>
+        <Footer />
+      </div>
+    </ReactQueryHydrate>
+  );
+};
+
+export default page;

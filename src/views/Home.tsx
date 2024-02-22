aa00d55ed974e2b7fe5630dc78093374436843e7
thanhhung <thanhhungdeveloper@gmail.com>
2024-02-22 16:36:22 +0700

    add shortcuts component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,8 @@
-import Labels from "@/components/home/Labels";
 import Slider from "@/components/home/Slider";
 import Navbar from "@/components/navbar/Navbar";
 import ProductsLayout from "@/components/home/ProductsLayout";
 import Footer from "@/components/footer/Footer";
+import ShortCuts from "@/components/home/Shorcuts";
 
 export default function Home() {
 
@@ -13,9 +13,10 @@ export default function Home() {
         <header className="py-8 my-4 md:my-0 md:py-16">
           <Slider />
         </header>
-        <Labels />
-        <ProductsLayout componentName="Featured Products" />
-        <ProductsLayout componentName="Best Seller" />
+        {/* <Labels /> */}
+        <ShortCuts />
+        <ProductsLayout componentName="Top sale items" />
+        <ProductsLayout componentName="Top rating products" />
       </div>
       <Footer />
     </main>

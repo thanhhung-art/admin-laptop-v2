cbcb45c420368d14430f6ebab704cff957a95d25
thanhhung <thanhhungdeveloper@gmail.com>
2024-02-22 16:30:29 +0700

    update review component

--- src/components/product/Reviews.tsx
+++ src/components/product/Reviews.tsx
@@ -1,59 +1,99 @@
-import ImageReview from "@/images/81nyJxacjEL._UC175,175_CACC,175,175_.jpg";
+"use client";
+
 import Image from "next/image";
-import StarIcon from "@/icons/StarIcon";
+import { useQuery } from "@tanstack/react-query";
+import { getReviews } from "@/lib/axios";
+import { Fragment, useMemo } from "react";
+import StarIconEmpty from "@/icons/StarIconEmpty";
+import { GetReviews } from "@/utils/keys";
 
-const Reviews = () => {
+interface IProps {
+  productId: string;
+}
+
+const Reviews = ({ productId }: IProps) => {
+  const { data } = useQuery([GetReviews], () => getReviews(productId));
+
   return (
-    <section className="bg-white rounded-md p-4 pb-0 flex-1 order-2 md:order-none">
-      <h2 className="text-xl">Reviews</h2>
+    <section className="bg-white rounded-md p-8 pb-0 flex-1 order-2 md:order-none">
+      <div className="flex gap-2 items-center">
+        <h2 className="text-xl">Reviews</h2>
+        { data && <h4>({data.data.length})</h4>}
+      </div>
       <ul className="mt-4">
-        <Child
-          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, hic
-              saepe et quaerat ipsa cumque in numquam corporis dolores,
-              perferendis a autem alias eaque aut fuga amet suscipit. Expedita,
-              ex."
-          image={true}
-        />
-
-        <Child />
+        {data &&
+          data.data.map((review) => (
+            <Fragment key={review._id}>
+              <Child
+                message={review.review}
+                images={review.images}
+                reviewDate={review.createdAt}
+                userRating={review.rating}
+              />
+            </Fragment>
+          ))}
       </ul>
     </section>
   );
 };
 
-function Child({ message, image }: { message?: string; image?: boolean }) {
+function Child({
+  message,
+  images,
+  reviewDate,
+  userRating,
+}: {
+  message?: string;
+  images?: string[];
+  reviewDate: string;
+  userRating: number;
+}) {
+  const dateCreated = useMemo(() => {
+    const dateObj = new Date(reviewDate);
+    const dateReformated = dateObj.toLocaleDateString(navigator.language, {
+      year: "numeric",
+      day: "2-digit",
+      month: "2-digit",
+    });
+    return `${dateReformated}`;
+  }, [reviewDate]);
+
   return (
     <li className="flex gap-2 md:gap-4 mb-4 md:mb-8">
-      <div className="pt-2">
-        <div className="h-8 w-8 rounded-full bg-gray-700"></div>
-      </div>
-
       <div>
         <div className="flex gap-4 items-center">
           <h3 className="font-semibold">John Doe</h3>
           <ul className="flex">
-            {[1, 2, 3, 4, 5].map((e) => (
-              <li key={e} className="ml-1">
-                <StarIcon w={16} h={16} />
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <li key={rating} className="">
+                <StarIconEmpty
+                  color={userRating + 1 > rating ? "#ff9238" : "none"}
+                />
               </li>
             ))}
           </ul>
         </div>
-        <p className="text-[12px] text-gray-600">United States on June 15, 2023</p>
-        <p className="text-gray-600 text-[12px] mb-2">Product quantity: good</p>
+
+        <p className="text-gray-600 text-[12px]">Product quantity: good</p>
 
         {message !== "" && <p className="text-sm">{message}</p>}
 
-        {image && (
-          <div className="mt-2">
-            <Image src={ImageReview} alt="image review" width={70} />
+        {images?.length && images.length > 0 && (
+          <div className="mt-2 flex gap-1">
+            {images.map((image, i) => (
+              <div key={i} className="relative w-16 h-16 mb-1">
+                <Image
+                  src={image}
+                  alt="image review"
+                  fill
+                  className="rounded"
+                />
+              </div>
+            ))}
           </div>
         )}
 
-        <div className="mt-2 md:mt-4 flex gap-8 items-center">
-          <span>like</span>
-          <button>reply</button>
-        </div>
+        <p className="text-[12px] text-gray-600">Reviewed {dateCreated}</p>
       </div>
     </li>
   );

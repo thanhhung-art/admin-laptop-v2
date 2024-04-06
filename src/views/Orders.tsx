76cfa638edb893765f209fb13736d0316b61a65a
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-06 16:21:01 +0700

    improvement: use param as a state

--- src/views/Orders.tsx
+++ src/views/Orders.tsx
@@ -7,31 +7,43 @@ import { IOrder } from "@/types/order";
 import { IProductInCart } from "@/types/product";
 import { GetOrdersByPhone } from "@/utils/keys";
 import { useQuery } from "@tanstack/react-query";
-import { createRef, useEffect, useRef, useState } from "react";
+import { ChangeEvent, createRef, useEffect, useRef, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const OrdersPage = () => {
+  const params = useSearchParams();
+  const phone = params.get("phone");
   const phoneRef = createRef<HTMLInputElement>();
   const [currProducts, setCurrProducts] = useState<IProductInCart[]>([]);
   const [sendData, setSendData] = useState(false);
   const dialogRef = createRef<HTMLDialogElement>();
   const openModalRef = createRef<HTMLButtonElement>();
+  const router = useRouter();
+  const [selectOption, setSelectOption] = useState<string>("all");
   const currOrder = useRef({
     username: "",
     id: "",
     customerPhone: "",
     reviewed: false,
-  })
+  });
 
   const { data, isLoading, isFetched, refetch } = useQuery(
-    [GetOrdersByPhone],
-    () => getOrdersByPhone(phoneRef.current ? phoneRef.current.value : null),
+    [GetOrdersByPhone, phone],
+    () => getOrdersByPhone(phone),
     {
-      enabled:
-        typeof phoneRef.current?.value === "string" &&
-        phoneRef.current.value.length === 10 &&
-        phoneRef.current.value.trim() !== "",
+      enabled: !!phone && phone.trim().length > 9,
     }
   );
+
+  const handleOpenOrders = () => {
+    if (phoneRef.current) {
+      const temp = phoneRef.current.value.trim();
+      if (temp.length > 9) {
+        router.replace(`?phone=${temp}`);
+        refetch();
+      }
+    }
+  };
 
   const handleOpenReview = (
     products: IProductInCart[],
@@ -60,13 +72,15 @@ const OrdersPage = () => {
     handleCloseModal();
   };
 
+  const handleStateOrdersSelected = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectOption(e.target.value);
+  };
+
   useEffect(() => {
-    if (sessionStorage.getItem("customer_phone") && phoneRef.current) {
-      const phone: string = sessionStorage.getItem("customer_phone") as string;
+    if (phoneRef.current && phone) {
       phoneRef.current.value = phone;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [phoneRef, phone]);
 
   return (
     <div className="max-w-7xl m-auto my-8">
@@ -78,14 +92,12 @@ const OrdersPage = () => {
             placeholder="Type your phone number"
             className="px-2 py-1 rounded flex-1 outline-none text-sm"
             ref={phoneRef}
+            onKeyDown={(e) => {
+              e.code === "Enter" && handleOpenOrders();
+            }}
           />
           <button
-            onClick={() =>
-              phoneRef.current?.value.trim() !== "" &&
-              phoneRef.current?.value.length === 10
-                ? refetch()
-                : undefined
-            }
+            onClick={handleOpenOrders}
             className="p-2 rounded bg-blue-500 text-white disabled:bg-gray-500 hover:bg-blue-600 active:bg-blue-700"
           >
             search
@@ -102,6 +114,7 @@ const OrdersPage = () => {
                 name="status orders"
                 id="type"
                 className="rounded"
+                onChange={handleStateOrdersSelected}
               >
                 <option value="all">all</option>
                 <option value="delivering">delivering</option>
@@ -111,56 +124,67 @@ const OrdersPage = () => {
           </div>
           <section className="flex flex-col gap-8">
             {data &&
-              data.data.map((order: IOrder) => {
-                const dateObj = new Date(order.createdAt as string);
-                const hours = dateObj.getHours();
-                const minutes = dateObj.getMinutes();
-                const dateReformated = dateObj.toLocaleDateString(
-                  navigator.language,
-                  {
-                    year: "numeric",
-                    day: "2-digit",
-                    month: "2-digit",
+              data.data
+                .filter((order) => {
+                  switch (selectOption) {
+                    case "delivering":
+                      return order.status === "pending";
+                    case "delivered":
+                      return order.status === "shipped";
+                    default:
+                      return order;
                   }
-                );
+                })
+                .map((order: IOrder) => {
+                  const dateObj = new Date(order.createdAt as string);
+                  const hours = dateObj.getHours();
+                  const minutes = dateObj.getMinutes();
+                  const dateReformated = dateObj.toLocaleDateString(
+                    navigator.language,
+                    {
+                      year: "numeric",
+                      day: "2-digit",
+                      month: "2-digit",
+                    }
+                  );
 
-                return (
-                  <div key={order._id} className="bg-white rounded-lg p-4">
-                    <div className="flex justify-between">
-                      <h3 className="font-semibold text-lg">
-                        Order #{order._id}
-                      </h3>
-                      <p className="text-sm text-gray-600">
-                        Order placed {hours}: {minutes} - {dateReformated}
-                      </p>
+                  return (
+                    <div key={order._id} className="bg-white rounded-lg p-4">
+                      <div className="flex justify-between">
+                        <h3 className="font-semibold text-lg">
+                          Order #{order._id?.slice(0, 10)}
+                        </h3>
+                        <p className="text-sm text-gray-600">
+                          Order placed {hours}: {minutes} - {dateReformated}
+                        </p>
+                      </div>
+                      <Products products={order.products} />
+                      <div className="flex justify-between items-center">
+                        <h6 className="font-semibold">
+                          Status{" "}
+                          <span className="text-green-700 ml-2">
+                            {order.status}
+                          </span>
+                        </h6>
+                        <button
+                          onClick={() =>
+                            handleOpenReview(
+                              order.products,
+                              order.username,
+                              order._id || "",
+                              order.phone,
+                              order.reviewed
+                            )
+                          }
+                          className="bg-blue-500 py-1 px-2 rounded text-white hover:bg-blue-600 active:bg-blue-700"
+                          ref={openModalRef}
+                        >
+                          {order.reviewed ? "Edit review" : "write review"}
+                        </button>
+                      </div>
                     </div>
-                    <Products products={order.products} />
-                    <div className="flex justify-between items-center">
-                      <h6 className="font-semibold">
-                        Status{" "}
-                        <span className="text-green-700 ml-2">
-                          {order.status}
-                        </span>
-                      </h6>
-                      <button
-                        onClick={() =>
-                          handleOpenReview(
-                            order.products,
-                            order.username,
-                            order._id || "",
-                            order.phone,
-                            order.reviewed
-                          )
-                        }
-                        className="bg-blue-500 py-1 px-2 rounded text-white hover:bg-blue-600 active:bg-blue-700"
-                        ref={openModalRef}
-                      >
-                        {order.reviewed ? "Edit review" : "write review"}
-                      </button>
-                    </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
           </section>
         </section>
       )}

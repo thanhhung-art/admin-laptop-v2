5bec8eec640b94174d2502959a4101647db4f10b
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-10 16:43:31 +0700

    add handleResize and event resize to component

--- src/hooks/isMobile.ts
+++ src/hooks/isMobile.ts
@@ -5,16 +5,28 @@ export default function useMobile() {
   const [isTable, setIsTable] = useState(false);
 
   useEffect(() => {
-    if (window && window.innerWidth < 768) {
-      setIsMobile(true);
-      setIsTable(false)
-    } else if (window && window.innerWidth < 1200 && window.innerWidth > 767) {
-      setIsTable(true)
-      setIsMobile(false)
-    } else {
-      setIsMobile(false)
-      setIsTable(false)
-    }
+    const handleResize = () => {
+      if (window && window.innerWidth < 768) {
+        setIsMobile(true);
+        setIsTable(false);
+      } else if (
+        window &&
+        window.innerWidth < 1200 &&
+        window.innerWidth > 767
+      ) {
+        setIsTable(true);
+        setIsMobile(false);
+      } else {
+        setIsMobile(false);
+        setIsTable(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return { isMobile, isTable };

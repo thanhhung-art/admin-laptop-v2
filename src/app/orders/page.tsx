6cf8a32aaf9040918ba49e724fcfb18a4de1e755
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-17 19:42:49 +0700

    improvement - use getOrdersByPhone instead getOrder

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,15 +1,21 @@
 import { ReactQueryHydrate } from "@/components/ReactQueryHydrate";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-import { getOrders } from "@/lib/axios";
+import { getOrdersByPhone } from "@/lib/axios";
 import { queryClient } from "@/lib/reactQuery/queryClient";
-import { GetOrders } from "@/utils/keys";
 import OrdersPage from "@/views/Orders";
 import { dehydrate } from "@tanstack/react-query";
 
-const page = async () => {
+const page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const queryClientLocal = queryClient();
-  await queryClientLocal.prefetchQuery([GetOrders], getOrders);
+  // searchParams["phone"] &&
+  //   (await queryClientLocal.prefetchQuery([getOrdersByPhone], () =>
+  //     getOrdersByPhone(searchParams["phone"] || null)
+  //   ));
   const dehydratedState = dehydrate(queryClientLocal);
 
   return (

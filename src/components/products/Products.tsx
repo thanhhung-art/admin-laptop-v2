d1f83472ba866a83815308c5567f1b58120f33dd
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-19 15:01:20 +0700

    improvement - remove unuse code

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,7 +1,7 @@
 import { InfiniteData } from "@tanstack/react-query";
 import { IProduct } from "@/types/product";
 import Card from "../Card";
-import { useMemo } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import useMobile from "@/hooks/isMobile";
 interface IProps {
   pages:
@@ -11,11 +11,10 @@ interface IProps {
       }>
     | undefined;
   isLoading: boolean;
-  filter?: string;
-  currentPrice: "up" | "down" | "none";
 }
 
-const Products = ({ pages, isLoading, filter, currentPrice }: IProps) => {
+const Products = ({ pages, isLoading }: IProps) => {
+  const [sortValue, setSortValue] = useState<"none" | "up" | "down">("none");
   const { isMobile } = useMobile();
   const products = useMemo(() => {
     if (!pages) return [];
@@ -25,31 +24,48 @@ const Products = ({ pages, isLoading, filter, currentPrice }: IProps) => {
     }, [] as IProduct[]);
   }, [pages]);
 
+  const handleSortProducts = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortValue(e.target.value as "none" | "up" | "down");
+  };
+
   if (products.length === 0)
     return <div className="text-center text-white">No products found</div>;
 
   return (
-    <ul className="max-w-7xl m-auto grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-4 justify-between mt-4">
-      {products
-        .sort((a, b) => {
-          if (currentPrice === "up") return a.price - b.price;
-          if (currentPrice === "down") return b.price - a.price;
-          return 0;
-        })
-        .map((product) => (
-          <li key={product._id}>
-            <Card
-              product={product}
-              letterQuantity={isMobile ? 120 : 180 }
-              imageHeight={{ sm: 'min-h-[130px]', md: 'min-h-[200px]' }}
-              type="small"
-              fontSize={{ sm: "text-sm", md: "text-base" }}
-              colorsCenter
-              starsCenter
-            />
-          </li>
-        ))}
-    </ul>
+    <>
+      <div className="max-w-7xl m-auto flex justify-end">
+        <select
+          title="sort products"
+          className="outline-none text-sm rounded"
+          onChange={handleSortProducts}
+        >
+          <option value="none">none</option>
+          <option value="up">price up</option>
+          <option value="down">price down</option>
+        </select>
+      </div>
+      <ul className="max-w-7xl m-auto grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-4 justify-between mt-4">
+        {products
+          .toSorted((a, b) => {
+            if (sortValue === "up") return a.price - b.price;
+            if (sortValue === "down") return b.price - a.price;
+            return 0;
+          })
+          .map((product) => (
+            <li key={product._id}>
+              <Card
+                product={product}
+                letterQuantity={isMobile ? 120 : 180}
+                imageHeight={{ sm: "min-h-[130px]", md: "min-h-[200px]" }}
+                type="small"
+                fontSize={{ sm: "text-sm", md: "text-base" }}
+                colorsCenter
+                starsCenter
+              />
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 

0b073cd58dd940484410a9d670b5f7d2336430a5
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-20 20:25:54 +0700

    remove get user function

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,6 @@
 import { IOrder } from "@/types/order";
 import { IProduct, IProductInSearch } from "@/types/product";
 import { IReviewFull } from "@/types/reviews";
-import { IUser } from "@/types/user";
 import axios from "axios";
 
 const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
@@ -39,23 +38,6 @@ export const getProductsInfinity = async (
 export const getProduct = async (id: string) => {
   const res = await Fetch(`/products/${id}`);
   return res.data as unknown as { data: IProduct; msg: string };
-};
-
-export const editProfile = async (data: {
-  userId: string;
-  email?: string;
-  phone?: string;
-  username?: string;
-  address?: string;
-  address2?: string;
-}) => {
-  const res = await Fetch.put("/users/" + data.userId);
-  return res.data as unknown as { data: IUser; msg: string };
-};
-
-export const getUser = async (id: string) => {
-  const res = await Fetch(`/users/${id}`);
-  return res.data as unknown as { data: IUser; msg: string };
 };
 
 export const getSearch = async (keyword: string | undefined) => {

9da3767ce290e28d540a01c8f961b79925a2b4e4
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-24 05:13:27 +0700

    improvement - fetch queries on server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
-import { ReactQueryHydrate } from '@/components/ReactQueryHydrate';
-import { getProductsInfinity } from '@/lib/axios';
-import { queryClient } from '@/lib/reactQuery/queryClient';
-import { GetProductsInfinity } from '@/utils/keys';
-import Home from '@/views/Home'
-import { dehydrate } from '@tanstack/react-query';
+import { ReactQueryHydrate } from "@/components/ReactQueryHydrate";
+import { getProducts } from "@/lib/axios";
+import { queryClient } from "@/lib/reactQuery/queryClient";
+import {
+  GetFeaturedProducts,
+  GetTopRatingProducts,
+  GetTopSellProducts,
+} from "@/utils/keys";
+import Home from "@/views/Home";
+import { dehydrate } from "@tanstack/react-query";
 
 export default async function page() {
   const queryClientLocal = queryClient();
-  await queryClientLocal.prefetchInfiniteQuery([GetProductsInfinity], ({ pageParam = 0 }) => getProductsInfinity(pageParam));
+  await Promise.all([
+    queryClientLocal.prefetchQuery([GetFeaturedProducts], () =>
+      getProducts("featured")
+    ),
+    queryClientLocal.prefetchQuery([GetTopSellProducts], () =>
+      getProducts("top_sell_products")
+    ),
+    queryClientLocal.prefetchQuery([GetTopRatingProducts], () =>
+      getProducts("top_rating_products")
+    ),
+  ]);
+
   const dehydratedState = dehydrate(queryClientLocal);
 
   return (
     <ReactQueryHydrate state={dehydratedState}>
       <Home />
-    </ReactQueryHydrate> 
-  )
+    </ReactQueryHydrate>
+  );
 }

0c1005a00f6999ffa00826c9f3d18c2d695fdbed
thanhhung <thanhhungdeveloper@gmail.com>
2024-05-06 14:19:26 +0700

    style - use css grid instead table in shortcuts

--- src/components/home/Shorcuts.tsx
+++ src/components/home/Shorcuts.tsx
@@ -1,44 +1,49 @@
 import Link from "next/link";
 
-const shortcuts = [
-  ["macbook", "acer", "asus", "lenovo", "dell", "hp", "msi"],
-  [
-    "gigabyte",
-    "microsoft",
-    "gaming",
-    "office, study",
-    "thin, light",
-    "graphic",
-    "coding",
-  ],
+const brands = [
+  "macbook",
+  "acer",
+  "asus",
+  "lenovo",
+  "dell",
+  "hp",
+  "msi",
+  "gigabyte",
+  "microsoft",
+];
+
+const categories = [
+  "gaming",
+  "office, study",
+  "thin, light",
+  "graphic",
+  "coding",
 ];
 
 const ShortCuts = () => {
-
   return (
     <section className=" max-w-7xl m-auto bg-white rounded-lg p-4 md:p-8 mb-8 md:mb-16">
       <div className="overflow-auto">
-        <table className="w-full border">
-          <tbody>
-            <tr>
-              {shortcuts[0].map((shortcut) => (
-                <td
-                  className="p-4 md:p-12 cursor-pointer border text-center hover:bg-slate-200 font-semibold"
-                  key={shortcut}
-                >
-                  {shortcut}
-                </td>
-              ))}
-            </tr>
-            <tr>
-              {shortcuts[1].map((shortcut) => (
-                <td className="p-4 md:p-12 border hover:bg-slate-200 font-semibold" key={shortcut}>
-                  <Link href={`/products?filter=${shortcut}`}>{shortcut}</Link>
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
+        <div className=" grid grid-cols-7 items-center justify-center overflow-auto] min-w-[840px] md:min-w-[1200px]">
+          {[...brands, ...categories].map((e) => {
+            let param = "";
+            if (brands.includes(e)) {
+              param = `?brand=${e}`;
+            } else if (categories.includes(e)) {
+              param = `?category=${e}`;
+            }
+            return (
+              <div
+                className="border cursor-pointer hover:bg-slate-200 active:bg-slate-300"
+                key={e}
+              >
+                <Link href={`/products${param}`}>
+                  <p className="text-center font-semibold text-sm md:text-base p-4 md:p-6">{e}</p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
